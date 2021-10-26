7773d02405cba1c6851cd1396c09ee2c84cba43c
Anton <anton.estum@gmail.com>
2021-10-26 19:12:02 +0300

    Init refine

--- lib/swagger_yard/rails/engine.rb
+++ lib/swagger_yard/rails/engine.rb
@@ -11,8 +11,8 @@ module SwaggerYard
 
       initializer "swagger_yard-rails.paths" do |app|
         SwaggerYard.configure do |config|
-          config.controller_path ||= ::Rails.root + 'app/controllers/**/*'
-          config.model_path      ||= ::Rails.root + 'app/models/**/*'
+          config.controller_path ||= ::Rails.root/'app/controllers/**/*'
+          config.model_path      ||= ::Rails.root/'app/models/**/*'
 
           config.path_discovery_function = RouteInspector.discovery_function
         end
--- swagger_yard-rails.gemspec
+++ swagger_yard-rails.gemspec
@@ -27,7 +27,7 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
-  spec.add_dependency "rails", ">= 3.2.8"
+  spec.add_dependency "railties", ">= 3.2.8"
   spec.add_dependency "swagger_yard", ">= 1.0.0"
 
   spec.add_development_dependency "bundler", "~> 1.10"
